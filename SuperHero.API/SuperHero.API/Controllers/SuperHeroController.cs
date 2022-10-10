cec3c59ba72438e0a894596820f6271fbfb811a7
Nguyễn Hữu Đức <huuduc0804@gmail.com>
2022-10-10 09:08:55 +0700

    Initial

---
+++ SuperHero.API/SuperHero.API/Controllers/SuperHeroController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SuperHero.API.Database;
+using SuperHero.API.Models;
+
+namespace SuperHero.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private readonly AppDatabase database;
+
+        public SuperHeroController(AppDatabase database)
+        {
+            this.database = database;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHeroModel>>> GetSuperHeroes()
+        {
+            return Ok(await database.SuperHeroes.ToListAsync());
+        }
+        [HttpPost]
+        public async Task<ActionResult<SuperHeroModel>> CreateSuperHero(SuperHeroModel hero)
+        {
+            database.SuperHeroes.Add(hero);
+            await database.SaveChangesAsync();
+            return Ok(hero);
+        }
+        [HttpPut]
+        public async Task<ActionResult<SuperHeroModel>> UpdateSuperHero(SuperHeroModel hero)
+        {
+            var dbHero = await database.SuperHeroes.FindAsync(hero.Id);
+            if (dbHero != null)
+            {
+                dbHero.Name = hero.Name;
+                dbHero.FirstName = hero.FirstName;
+                dbHero.LastName = hero.LastName;
+                dbHero.Place = hero.Place;
+                await database.SaveChangesAsync();
+                return Ok(hero);
+            }
+            else
+            {
+                return BadRequest($"Can't find hero with ID : {hero.Id}");
+            }
+        }
+        [HttpDelete("{Id}")]
+        public async Task<ActionResult<SuperHeroModel>> DeleteSuperHero(int Id)
+        {
+            var dbHero = await database.SuperHeroes.FindAsync(Id);
+            if(dbHero!=null)
+            {
+                database.SuperHeroes.Remove(dbHero);
+                await database.SaveChangesAsync();
+                return Ok(dbHero);
+            }
+            else
+            {
+                return BadRequest($"Can't find hero with ID : {Id}");
+            }
+        }
+    }
+}
---
+++ SuperHero.API/SuperHero.API/Database/AppDatabase.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperHero.API.Models;
+
+namespace SuperHero.API.Database
+{
+    public class AppDatabase : DbContext
+    {
+        public AppDatabase(DbContextOptions<AppDatabase> options) : base(options)
+        { 
+        }
+        public DbSet<SuperHeroModel> SuperHeroes => Set<SuperHeroModel>();
+    }
+}
